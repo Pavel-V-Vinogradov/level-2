205105b58d1436d8b3aef8f9699c361df5a99e61
Pavel.V.Vinogradov <PavelBuilder@ya.ru>
2022-04-10 17:48:25 +0300

    Java. Уровень 2 Урок 6. Работа с сетью

---
+++ src/main/java/ru/geekbrains/level_2/lesson_6/ClientStarter.java
@@ -0,0 +1,7 @@
+package ru.geekbrains.level_2.lesson_6;
+
+public class ClientStarter {
+    public static void main(String[] args) {
+        new Client().start();
+    }
+}
---
+++ src/main/java/ru/geekbrains/level_2/lesson_6/ServerBootUp.java
@@ -0,0 +1,7 @@
+package ru.geekbrains.level_2.lesson_6;
+
+public class ServerBootUp {
+    public static void main(String[] args) {
+        new Server().start();
+    }
+}
